b91917fac6ac219b0cf2157b81d6544f678c713d
praveenm <manukonda87@outlook.com>
2025-02-21 23:53:09 +0530

    Added Responsive Layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,27 @@
-import { Button } from "@chakra-ui/react";
+import { Grid, GridItem } from "@chakra-ui/react";
+
 function App() {
-  return <Button>Click me</Button>;
+  return (
+    <>
+      <Grid
+        templateAreas={{ base: `"nav" "main"`, lg: `"nav nav" "aside main"` }}
+      >
+        <GridItem area="nav" bg="coral">
+          nav
+        </GridItem>
+        <GridItem
+          area="aside"
+          bg="gold"
+          display={{ base: "none", lg: "block" }}
+        >
+          aside
+        </GridItem>
+        <GridItem area="main" bg="orange">
+          main
+        </GridItem>
+      </Grid>
+    </>
+  );
 }
 
 export default App;
